b74e8909c1056308449140393ce783a962d9168a
callme1ngin <antones270@gmail.com>
2025-11-22 18:10:44 +0700

    Allowed hosts changed

--- panzy/settings.py
+++ panzy/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['185.180.230.157', 'localhost', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
